304a2b79267a9c1a66c763ba0a1574154380f6ae
zackarno <zackarno@gmail.com>
2024-03-13 17:36:36 -0400

    split qmd files into 2 as can't seem to parameterize subtitle

--- flood/render_moz_flooding_report.R
+++ flood/render_moz_flooding_report.R
@@ -1,17 +1,15 @@
 
-file_name_quarto <- "04_moz_flash_updates_viz_country_wide.qmd"
+file_name_country_wide <- "04_moz_flash_updates_viz_country_wide.qmd"
+file_name_subset <- "05_moz_flash_updates_viz_subset.qmd"
 
 run_date <- format(Sys.Date(),"%Y%m%d")
 quarto::quarto_render(input = file.path("flood",file_name_quarto),
-                      execute_params = list("aoi" = "country_wide",
-                                            "subby"="Country Wide Analysis")
+                      execute_params = list("aoi" = "country_wide"
+                                            )
                       )
 
 
-quarto::quarto_render(input = file.path("flood",file_name_quarto),
-                      execute_params = list("aoi" = "subset1",
-                                            "subby"="Analysis of Sofala & Inhambane Provinces")
-                      # output_file = "flood\test.html",
-                      # output_format = "html"
+quarto::quarto_render(input = file.path("flood",file_name_subset),
+                      execute_params = list("aoi" = "subset1")
                       )
 
