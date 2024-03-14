94970ad5abbda15ba9baafea48c720cef5f1ee0d
zackarno <zackarno@gmail.com>
2024-03-14 15:16:58 -0400

    add update_data to params so it can be controlled externally at runtime

--- flood/render_moz_flooding_report.R
+++ flood/render_moz_flooding_report.R
@@ -4,12 +4,26 @@ file_name_subset <- "05_moz_flash_updates_viz_subset.qmd"
 
 run_date <- format(Sys.Date(),"%Y%m%d")
 quarto::quarto_render(input = file.path("flood",file_name_quarto),
-                      execute_params = list("aoi" = "country_wide"
-                                            )
+                      execute_params = list(
+                        "aoi" = "country_wide",
+                        "update_date" = "true"
                       )
+)
 
 
 quarto::quarto_render(input = file.path("flood",file_name_subset),
-                      execute_params = list("aoi" = "subset1")
+                      execute_params = list(
+                        "aoi" = "subset1",
+                        "update_data"="true"
+                        )
                       )
+
+
+
+# quarto::quarto_render(input = file.path("flood",file_name_quarto),
+#                       execute_params = list(
+#                         "aoi" = "country_wide",
+#                         "update_local" = T
+#                       )
+# )
 
