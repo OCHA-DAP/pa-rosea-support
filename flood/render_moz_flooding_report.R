69420c89fe7aa290868e37b16b35b36a46e94603
zackarno <zackarno@gmail.com>
2024-03-14 20:33:26 -0400

    complete() c('SFED','MFED') prior to plotting for more accurate representation of daily values

--- flood/render_moz_flooding_report.R
+++ flood/render_moz_flooding_report.R
@@ -3,10 +3,10 @@ file_name_country_wide <- "04_moz_flash_updates_viz_country_wide.qmd"
 file_name_subset <- "05_moz_flash_updates_viz_subset.qmd"
 
 run_date <- format(Sys.Date(),"%Y%m%d")
-quarto::quarto_render(input = file.path("flood",file_name_quarto),
+quarto::quarto_render(input = file.path("flood",file_name_country_wide),
                       execute_params = list(
                         "aoi" = "country_wide",
-                        "update_date" = "true"
+                        "update_data" = "true"
                       )
 )
 
