a41e4c56d3165ff14841ad704e63f64f340d9476
Pauline Wairimu <paunimo@gmail.com>
2024-03-21 19:06:05 +0300

    adding frequency analysis

--- drought/moz/2023-2024-season-review.R
+++ drought/moz/2023-2024-season-review.R
@@ -1,6 +1,8 @@
 ## libraries
 library(tidyverse)
 library(sf)
+library(ggrepel)
+library(fitdistrplus)
 library(gghdx)
 gghdx()
 
@@ -20,10 +22,12 @@ moz_rain_adm1 <- moz_rain_data %>%
                select(ADM2_PCODE, ADM1_PCODE, ADM1_PT)), 
             by = "ADM2_PCODE", suffix = c("", "_shp")) %>%
   mutate(month_total = as.numeric(r1h),
-         month_total_avg = as.numeric(r1h_avg)) %>%
+         month_total_avg = as.numeric(r1h_avg),
+         month_anom = as.numeric(r1q)) %>%
   group_by(ADM1_PCODE, ADM1_PT, date) %>%
   summarise(adm1_tot_med = median(month_total, na.rm = T),
-            adm1_avg_med = median(month_total_avg, na.rm = T)) %>%
+            adm1_avg_med = median(month_total_avg, na.rm = T),
+            adm1_anom_med = median(month_anom, na.rm = T)) %>%
   mutate(dekad_start = substr(date, 9, 10),
          month = substr(date, 6, 7),
          month_name = month.name[as.numeric(month)],
@@ -36,31 +40,96 @@ moz_rain_adm1 <- moz_rain_data %>%
   mutate(month_name = factor(month_name, levels = moz_ssn),
          highlighted_ssn = case_when(
            season == "2015/2016" ~ season,
+           season == "2022/2023" ~ season,
            season == "2023/2024" ~ season,
-           .default = "Other Seasons"
-         ))
+           .default = "Other Seasons"),
+         ssn_values = case_when(
+           season == "2015/2016" ~ adm1_tot_med,
+           season == "2022/2023" ~ adm1_tot_med,
+           season == "2023/2024" ~ adm1_tot_med,
+           .default = adm1_avg_med))
 
+moz_subsets <- split(moz_rain_adm1, moz_rain_adm1$ADM1_PT)
+line_width <- c("2015/2016" = 0.8, "2022/2023" = 0.8, "2023/2024" = 0.8, "Other Seasons" = 0.5)
 
 ggplot(data = moz_rain_adm1) +
   geom_line(aes(x=month_name, y=adm1_tot_med, 
-                group=season, color=highlighted_ssn)) +
-  scale_color_manual(values = c("cyan", "maroon", "grey")) +
-  scale_size_manual(values = c(3, 3, 1)) +
-  labs(title="Total Monthly Rainfall by Province", x="Month", y="Total Monthly Rainfall(mm)") + 
+                group=season, color=highlighted_ssn, 
+                size=highlighted_ssn)) +
+  scale_color_manual(values = c("maroon", "steelblue", "darkgreen", "grey")) +
+  scale_size_manual(values = line_width, guide = "none") +
+  labs(title="Total Monthly Rainfall by Province", 
+       x="Month", y="Total Monthly Rainfall (mm)", color = "Seasons") + 
   facet_wrap(~ADM1_PT)
 
-moz_subsets <- split(moz_rain_adm1, moz_rain_adm1$ADM1_PT)
-line_width <- c("2015/2016" = 1, "2023/2024" = 1, "Other Seasons" = 0.5)
+ggplot(data = moz_rain_adm1) +
+  geom_line(aes(x=month_name, y=ssn_values, 
+                group=highlighted_ssn, color=highlighted_ssn, 
+                size=highlighted_ssn)) +
+  scale_color_manual(values = c("maroon", "steelblue", "#8B8000", "grey")) +
+  scale_size_manual(values = line_width, guide = "none") +
+  labs(title="Total Monthly Rainfall by Province", 
+       x="Month", y="Total Monthly Rainfall (mm)", color = "Seasons") + 
+  facet_wrap(~ADM1_PT)
+
+
 map(moz_subsets, 
     ~ ggplot(.x) +
       geom_line(aes(x=month_name, y=adm1_tot_med, 
                     group=season, color=highlighted_ssn, size=highlighted_ssn)) +
-      scale_color_manual(values = c("darkred", "navyblue", "grey")) +
+      scale_color_manual(values = c("maroon", "steelblue", "#8B8000", "grey")) +
       scale_size_manual(values = line_width, guide = "none") +
       labs(title=paste("Total Monthly Rainfall for ", unique(.x$ADM1_PT)), 
-           x="Month", y="Total Monthly Rainfall(mm)",
+           x="Month", y="Total Monthly Rainfall (mm)",
            color = "Seasons") +
       guides(colour = guide_legend(override.aes = list(size = 3)))) 
+
+
+## looking at anomalies
+moz_rain_adm1 %>%
+  filter(season == "2023/2024") %>%
+  ggplot() +
+    geom_line(aes(x=month_name, y=adm1_anom_med, 
+                  group=ADM1_PT, color=ADM1_PT), size = 1) +
+    geom_text_repel(data = moz_rain_adm1 %>%
+                filter(season == "2023/2024") %>%
+                filter(date == "2024-02-21"), 
+              aes(x = "February", y = adm1_anom_med, label = ADM1_PT), 
+            size = 4, hjust = 0, color = "black",
+            position = position_nudge(x = 0.02, y = 0.3)) +
+    labs(title="Monthly Rainfall Anomalies by Province", 
+         x="Month", y="Monthly Rainfall Anomalies (%)", color = "Provinces") +
+    guides(color = "none", size = "none")
+
+## ranking seasons based on total rainfall by month and overall
+moz_rank <- moz_rain_adm1 %>%
+  filter(month_name != "March") %>%
+  group_by(ADM1_PT, season) %>%
+  summarise(adm1_ssn_avg = sum(adm1_tot_med, na.rm = T)) %>%
+  group_by(season) %>%
+  summarise(moz_ssn_avg = median(adm1_ssn_avg, na.rm = T)) %>%
+  mutate(rank = rank(moz_ssn_avg))
+
+ggplot(moz_rank) + 
+  geom_bar(aes(x=season, y=moz_ssn_avg), stat = "identity") +
+  theme(axis.text.x = element_text(angle = 90)) +
+  labs(title=paste("Average Seasonal Rainfall"), 
+       x="Season", y="Average Season Rainfall (mm)")
+  
+View(moz_rain_adm1 %>%
+  group_by(ADM1_PT, season) %>%
+  summarise(adm1_ssn_avg = sum(adm1_tot_med, na.rm = T)))
+
+adm1_rank <- moz_rain_adm1 %>%
+  filter(month_name != "March") %>%
+  group_by(ADM1_PT, season) %>%
+  summarise(adm1_ssn_avg = sum(adm1_tot_med, na.rm = T)) %>%
+  mutate(rank = rank(adm1_ssn_avg),
+         mean = mean(adm1_ssn_avg),
+         sd = sd(adm1_ssn_avg),
+         prob = pnorm(adm1_ssn_avg, mean = mean, sd = sd),
+         rp = paste("1 in ", round(1/prob), " yr"))
+
 
 ## NDVI
 
